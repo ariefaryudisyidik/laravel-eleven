fc2e402a170e6f0d98cbc6a77db720d8ad340aa7
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-05-26 16:59:33 +0700

    refactor: Update posts view to use dynamic data

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -18,16 +19,46 @@ Route::get('/posts', function () {
         'title' => 'Blog',
         'posts' => [
             [
+                'id' => 1,
+                'slug' => 'article-title-1',
                 'title' => 'Article Title 1',
                 'author' => 'Arief Aryudi Syidik',
                 'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, repudiandae! Alias odit exercitationem ratione tempora, veritatis eum repellendus maxime esse. Officiis cum quos eveniet quas suscipit explicabo saepe ipsa maiores!',
             ],
             [
+                'id' => 2,
+                'slug' => 'article-title-2',
                 'title' => 'Article Title 2',
                 'author' => 'Arief Aryudi Syidik',
                 'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ex eos aut ipsum totam asperiores ab vel omnis explicabo rerum, inventore quo excepturi repellat fuga architecto, corrupti consequuntur perspiciatis. Sunt?',
             ],
         ],
+    ]);
+});
+
+Route::get('/posts/{slug}', function ($slug) {
+    $posts = [
+        [
+            'id' => 1,
+            'slug' => 'article-title-1',
+            'title' => 'Article Title 1',
+            'author' => 'Arief Aryudi Syidik',
+            'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, repudiandae! Alias odit exercitationem ratione tempora, veritatis eum repellendus maxime esse. Officiis cum quos eveniet quas suscipit explicabo saepe ipsa maiores!',
+        ],
+        [
+            'id' => 2,
+            'slug' => 'article-title-2',
+            'title' => 'Article Title 2',
+            'author' => 'Arief Aryudi Syidik',
+            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ex eos aut ipsum totam asperiores ab vel omnis explicabo rerum, inventore quo excepturi repellat fuga architecto, corrupti consequuntur perspiciatis. Sunt?',
+        ],
+    ];
+
+    $post = Arr::first($posts, fn($post) => $post['slug'] == $slug);
+
+    return view('post', [
+        'title' => 'Single Post',
+        'post' => $post,
     ]);
 });
 
