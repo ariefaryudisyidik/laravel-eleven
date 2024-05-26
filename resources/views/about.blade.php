d5f77b4b5bec8de27ceaa1087f70a0611e614672
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-05-26 16:02:55 +0700

    refactor: Update routes and views for home, about, blog, and contact pages

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,22 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>About page</title>
-    <link rel="stylesheet" href="/css/style.css">
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/blog">Blog</a>
-    <a href="/about">About</a>
-    <a href="/contact">Contact</a>
-    <h1>About Laravel Page</h1>
-    <h3>Owner: {{ $name }}</h3>
-    <img src="/img/laravel.png" width="100" alt="Laravel">
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot:title>
+    <h3 class="text-xl">About Page</h3>
+    <p>{{ $name }}</p>
+</x-layout>
