07da04334ba199fb3e119cc0b43b7943a072651c
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-05-27 08:25:33 +0700

    refactor: Update Post model to extend Eloquent Model

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,32 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Support\Arr;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class Post
+class Post extends Model
 {
-    public static function all(): array
-    {
-        return [
-            [
-                'id' => 1,
-                'slug' => 'article-title-1',
-                'title' => 'Article Title 1',
-                'author' => 'Arief Aryudi Syidik',
-                'body' => 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Provident, repudiandae! Alias odit exercitationem ratione tempora, veritatis eum repellendus maxime esse. Officiis cum quos eveniet quas suscipit explicabo saepe ipsa maiores!',
-            ],
-            [
-                'id' => 2,
-                'slug' => 'article-title-2',
-                'title' => 'Article Title 2',
-                'author' => 'Arief Aryudi Syidik',
-                'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Minus ex eos aut ipsum totam asperiores ab vel omnis explicabo rerum, inventore quo excepturi repellat fuga architecto, corrupti consequuntur perspiciatis. Sunt?',
-            ],
-        ];
-    }
+    use HasFactory;
 
-    public static function find($slug): array
-    {
-        return Arr::first(self::all(), fn($post) => $post['slug'] == $slug) ?? abort(404);
-    }
+    protected $fillable = ['title', 'author', 'slug', 'body'];
 }
