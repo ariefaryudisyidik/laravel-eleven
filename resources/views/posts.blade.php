1b11c869690ea76c82875c9fc879f3a6d20224a9
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-06-03 09:24:44 +0700

    refactor: Update posts and post views to display author and category as a link

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -5,14 +5,14 @@
             <a href="/posts/{{ $post['slug'] }}" class="hover:underline">
                 <h2 class="mb-1 text-3xl tracking-tight font-bold text-gray-900">{{ $post['title'] }}</h2>
             </a>
-            <div class="">
+            <div>
                 By
                 <a href="/authors/{{ $post->author->username }}" class="text-base text-gray-500 hover:underline">
                     {{ $post->author->name }}
                 </a>
                 in
-                <a href="#" class="text-base text-gray-500 hover:underline">
-                    Web Programming
+                <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">
+                    {{ $post->category->name }}
                 </a>
                 | {{ $post['created_at']->diffForHumans() }}
             </div>
