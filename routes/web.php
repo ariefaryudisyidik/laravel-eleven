1c108f41dad3d0f63ced0c796810cbebb9446196
ariefaryudisyidik <ariefaryudisyidik@gmail.com>
2024-06-30 07:55:58 +0700

    refactor: Handle N+1 in Routes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,11 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
+    $posts = Post::with('author', 'category')->latest()->get();
+
     return view('posts', [
         'title' => 'Blog',
-        'posts' => Post::all(),
+        'posts' => $posts,
     ]);
 });
 
@@ -31,16 +33,20 @@ Route::get('/posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('/authors/{user:username}', function (User $user) {
+    $posts = $user->posts->load('author', 'category');
+
     return view('posts', [
         'title' => count($user->posts).' Articles by '.$user->name,
-        'posts' => $user->posts,
+        'posts' => $posts,
     ]);
 });
 
 Route::get('/categories/{category:slug}', function (Category $category) {
+    $posts = $category->posts->load('author', 'category');
+
     return view('posts', [
         'title' => 'Articles in '.$category->name,
-        'posts' => $category->posts,
+        'posts' => $posts,
     ]);
 });
 
